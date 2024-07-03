2d11a1edd78104050c802b767b5713ed5657cdc1
Serticode <helpdeskforserticode@gmail.com>
2024-07-03 15:19:13 +0100

    Updated utils file to carry a getMaterialColour functionality.

--- lib/shared/utils/utils.dart
+++ lib/shared/utils/utils.dart
@@ -77,4 +77,30 @@ class AppUtils {
         return coloursInstance.grey900;
     }
   }
+
+  //!
+  //!
+  MaterialColor getMaterialColor({
+    required Color theColour,
+  }) {
+    final int red = theColour.red;
+    final int green = theColour.green;
+    final int blue = theColour.blue;
+    final int alpha = theColour.alpha;
+
+    final Map<int, Color> shades = {
+      50: Color.fromARGB(alpha, red, green, blue),
+      100: Color.fromARGB(alpha, red, green, blue),
+      200: Color.fromARGB(alpha, red, green, blue),
+      300: Color.fromARGB(alpha, red, green, blue),
+      400: Color.fromARGB(alpha, red, green, blue),
+      500: Color.fromARGB(alpha, red, green, blue),
+      600: Color.fromARGB(alpha, red, green, blue),
+      700: Color.fromARGB(alpha, red, green, blue),
+      800: Color.fromARGB(alpha, red, green, blue),
+      900: Color.fromARGB(alpha, red, green, blue),
+    };
+
+    return MaterialColor(theColour.value, shades);
+  }
 }
